a666bf41b12bb6d014ea1deef1f6b9f4477ba445
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-02-07 16:23:51 +0000

    [fotorama] Update zh_TW, thanks to : azq1231
    git-svn-id: http://piwigo.org/svn/extensions/Fotorama@27261 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Allows fullscreen'] = '允許全屏';
+$lang['Enables autoplay'] = '啟用自動播放';
+$lang['Enables loop'] = '啟用循環';
+$lang['Enables shadows'] = '允許陰影';
+$lang['How to fit an image'] = '如何適應圖像';
+$lang['Navigation style'] = '導航式';
+$lang['What transition to use'] = '相片切換特效';
+$lang['stop the slideshow'] = '停止幻燈片放映';
+?>
