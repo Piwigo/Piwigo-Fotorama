733da70ba13a5b30c0e0329f5c87a4379d35cdd6
ddtddt <ddtddt@piwigo.org>
2014-02-09 18:00:36 +0000

    [Trunk] - Fotorama - add cs_CZ - Česky – Czech - Thanks to webprostor
    git-svn-id: http://piwigo.org/svn/extensions/Fotorama@27291 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,11 @@
+<?php
+$lang['stop the slideshow'] = 'zastavit slideshow';
+$lang['Allows fullscreen'] = 'Povolit fullscreen';
+$lang['How to fit an image'] = 'Jakým způsobem zobrazit obraz';
+$lang['What transition to use'] = 'Který přechod se má použít';
+$lang['Navigation style'] = 'Typ vzhledu ovládání';
+$lang['Enables shadows'] = 'Zapnout stíny';
+$lang['Enables autoplay'] = 'Zapnout autoplay';
+$lang['Stops slideshow at any user action with the fotorama'] = 'Ukončit slideshow při jakékoliv akci návštěvníka s fotorama';
+$lang['Enables loop'] = 'Zapnout smyčku';
+?>
