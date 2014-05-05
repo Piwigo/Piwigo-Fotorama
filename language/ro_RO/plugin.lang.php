ec5ab2add57740abfb8c15631e4ae55597d38595
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-05-05 21:02:08 +0000

    [fotorama] Update ro_RO, thanks to : cheilenereinfo
    git-svn-id: http://piwigo.org/svn/extensions/Fotorama@28373 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ro_RO/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['How to fit an image'] = 'Cum sa se potriveasca o imaginea';
+$lang['Navigation style'] = 'Stil de navigare';
+$lang['stop the slideshow'] = 'opreste prezentarea';
+$lang['What transition to use'] = 'Tranzitia care sa se foloseasca';
+$lang['Stops slideshow at any user action with the fotorama'] = 'Opreste prezentare la orice actiune a utilizatorului cu fotorama';
+$lang['Only fullscreen mode'] = 'Doar modul pe ecran intreg';
+$lang['Fullscreen navigation style'] = 'Stil navigare pe ecran intreg';
+$lang['Enables shadows'] = 'Permite umbre';
+$lang['Enables loop'] = 'Permite rulare in bucla';
+$lang['Enables autoplay'] = 'Permite pornire automata';
+$lang['Allows fullscreen'] = 'Permite afisare pe ecran intreg';
