a4f308f6faee2eb00627c377ccfc02394db3d566
JanisV <JanisV@piwigo.org>
2014-09-04 08:53:26 +0000

    Up version
    git-svn-id: http://piwigo.org/svn/extensions/Fotorama@29399 68402e56-0260-453c-a942-63ccdbb3a9ee

--- main.inc.php
+++ main.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Fotorama
-Version: 2.7.i
+Version: 2.7.j
 Description: Fotorama based full-screen slideshow
 Plugin URI: http://piwigo.org/ext/extension_view.php?eid=727
 Author: JanisV
