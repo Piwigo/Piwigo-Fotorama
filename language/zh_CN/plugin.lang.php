d754fdfb4343d2230fee776ba8c7021071d06212
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-12 19:28:56 +0000

    [fotorama] Update zh_CN, thanks to : dennisyan
    git-svn-id: http://piwigo.org/svn/extensions/Fotorama@30026 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/zh_CN/plugin.lang.php
+++ language/zh_CN/plugin.lang.php
@@ -21,14 +21,24 @@
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
 $lang['stop the slideshow'] = '停止幻灯片';
-$lang['How to fit an image'] = '如何适应一幅图像';
-$lang['Navigation style'] = '导航方式';
+$lang['How to fit an image'] = '如何适应图片';
+$lang['Navigation style'] = '导航样式';
 $lang['Stops slideshow at any user action with the fotorama'] = '发生任何动作时停止幻灯片播放';
-$lang['What transition to use'] = '使用哪个过场方式';
+$lang['What transition to use'] = '使用何种过渡方式';
 $lang['Enables autoplay'] = '开启自动播放';
 $lang['Enables loop'] = '开启循环';
 $lang['Enables shadows'] = '开启阴影';
 $lang['Allows fullscreen'] = '允许全屏';
 $lang['Fullscreen navigation style'] = '全屏导航样式';
-$lang['Only fullscreen mode'] = '只在全屏模式';
-?>
+$lang['Only fullscreen mode'] = '仅全屏模式';
+$lang['Show caption with image title'] = '显示带有图片标题的文字说明';
+$lang['milliseconds'] = '毫秒';
+$lang['Add close button'] = '添加关闭按钮';
+$lang['Add image info button'] = '添加图片信息按钮';
+$lang['Replace picture page'] = '替换图片页';
+$lang['Resize to fit window'] = '调整尺寸以适应窗口';
+$lang['Thumbnail height (when present)'] = '缩略图高度（当其呈现时）';
+$lang['Thumbnail is a square (when present)'] = '缩略图为正方形（当其呈现时）';
+$lang['Use crossfade transition on click'] = '点击时启用淡入淡出过渡';
+$lang['Waiting time before loading a new image'] = '载入新图片时的等待时间';
+$lang['except an administrator'] = '管理员除外';
