16687cf49950ca0f2ed2ad13bbcb1e87dad16728
JanisV <JanisV@piwigo.org>
2014-11-05 09:24:29 +0000

    Fixed missed values in config
    git-svn-id: http://piwigo.org/svn/extensions/Fotorama@30368 68402e56-0260-453c-a942-63ccdbb3a9ee

--- admin.php
+++ admin.php
@@ -7,6 +7,8 @@ load_language('plugin.lang', FOTORAMA_PATH);
 
 if (isset($_POST['submit']))
 {
+  $old_conf = $conf;
+
   $conf['Fotorama'] = array(
     'shadows' => isset($_POST['shadows']),
     'only_fullscreen' => isset($_POST['only_fullscreen']),
@@ -24,18 +26,32 @@ if (isset($_POST['submit']))
     );
   if (isset($_POST['allowfullscreen']))
     $conf['Fotorama']['allowfullscreen'] = $_POST['allowfullscreen'];
+  else
+    $conf['Fotorama']['allowfullscreen'] = $old_conf['Fotorama']['allowfullscreen'];
   if (isset($_POST['fit']))
     $conf['Fotorama']['fit'] = $_POST['fit'];
+  else
+    $conf['Fotorama']['fit'] = $old_conf['Fotorama']['fit'];
   if (isset($_POST['transition']))
     $conf['Fotorama']['transition'] = $_POST['transition'];
+  else
+    $conf['Fotorama']['transition'] = $old_conf['Fotorama']['transition'];
   if (isset($_POST['nav']))
     $conf['Fotorama']['nav'] = $_POST['nav'];
+  else
+    $conf['Fotorama']['nav'] = $old_conf['Fotorama']['nav'];
   if (isset($_POST['fullscreen_nav']))
     $conf['Fotorama']['fullscreen_nav'] = $_POST['fullscreen_nav'];
+  else
+    $conf['Fotorama']['fullscreen_nav'] = $old_conf['Fotorama']['fullscreen_nav'];
   if (isset($_POST['thumbheight']))
     $conf['Fotorama']['thumbheight'] = $_POST['thumbheight'];
+  else
+    $conf['Fotorama']['thumbheight'] = $old_conf['Fotorama']['thumbheight'];
   if (isset($_POST['period']))
     $conf['Fotorama']['period'] = $_POST['period'];
+  else
+    $conf['Fotorama']['period'] = $old_conf['Fotorama']['period'];
 
   conf_update_param('Fotorama', serialize($conf['Fotorama']));
   $page['infos'][] = l10n('Information data registered in database');
