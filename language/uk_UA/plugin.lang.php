1350380262829f661f1f024a905aeb12f8cea1cc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-11-12 12:08:17 +0000

    [fotorama] Update uk_UA, thanks to m_alien
    git-svn-id: http://piwigo.org/svn/extensions/Fotorama@30448 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Use crossfade transition on click'] = 'Використовувати перехід crossfade при кліку мищі';
+$lang['What transition to use'] = 'Який перехід використовувати';
+$lang['except an administrator'] = 'крім адміністратора';
+$lang['Thumbnail is a square (when present)'] = 'Квадратні мініатюри (за їх наявності)';
+$lang['Thumbnail height (when present)'] = 'Висота мініатюр (за їх наявності)';
+$lang['Stops slideshow at any user action with the fotorama'] = 'Зупинити слайдшоу за будь-якої дії користувача fotorama';
+$lang['Show caption with image title'] = 'Показувати заголовок з назвою зображення';
+$lang['Resize to fit window'] = 'Змінити розмір до розміру вікна';
+$lang['Navigation style'] = 'Навігаційний стиль';
+$lang['How to fit an image'] = 'Як приладжувати зображення';
+$lang['Fullscreen navigation style'] = 'Повноекраний стиль навігації';
+$lang['milliseconds'] = 'мілісекунд';
+$lang['stop the slideshow'] = 'вимкнути слайдшоу';
+$lang['Waiting time before loading a new image'] = 'Час очікування до завантаження нового зображення';
+$lang['Only fullscreen mode'] = 'Тільки повноекранний режим';
+$lang['Enables shadows'] = 'Активує тіні';
+$lang['Enables loop'] = 'Активує цикллічне відтворення';
+$lang['Enables autoplay'] = 'Активує автозапуск';
+$lang['Allows fullscreen'] = 'Дозволяє повний екран';
+$lang['Add image info button'] = 'Додати кнопку інформації зображення';
+$lang['Add close button'] = 'Додати кнопку закриття';
