95b4ab98dbb050115ab2675085a21aecbe2a6931
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-12-15 21:13:56 +0000

    Update cs_CZ, thanks to marfig
    git-svn-id: http://piwigo.org/svn/extensions/Fotorama@30763 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/cs_CZ/plugin.lang.php
+++ language/cs_CZ/plugin.lang.php
@@ -8,4 +8,11 @@ $lang['Enables shadows'] = 'Zapnout stíny';
 $lang['Enables autoplay'] = 'Zapnout autoplay';
 $lang['Stops slideshow at any user action with the fotorama'] = 'Ukončit slideshow při jakékoliv akci návštěvníka s fotorama';
 $lang['Enables loop'] = 'Zapnout smyčku';
-?>
+$lang['Resize to fit window'] = 'Změnit velikost pro vyplnění okna';
+$lang['Waiting time before loading a new image'] = 'Vyčkávací čas před načtením nového obrázku';
+$lang['except an administrator'] = 'Vyjma administrátora';
+$lang['Add close button'] = 'Přidat tlačítko na zavření';
+$lang['Add image info button'] = 'Přidat tlačítko pro informace o obrázku';
+$lang['Fullscreen navigation style'] = 'Styl fulscreen navigace';
+$lang['Only fullscreen mode'] = 'Pouze fullscreen mód';
+$lang['milliseconds'] = 'milisekund';
