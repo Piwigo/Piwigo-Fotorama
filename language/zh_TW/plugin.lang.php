c32044373f23a0508a5ef0144c2dc9974e5f882e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-01-12 05:16:39 +0000

    Update zh_TW, thanks to azq1231
    git-svn-id: http://piwigo.org/svn/extensions/Fotorama@30885 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/zh_TW/plugin.lang.php
+++ language/zh_TW/plugin.lang.php
@@ -31,4 +31,14 @@ $lang['stop the slideshow'] = '停止幻燈片放映';
 $lang['Stops slideshow at any user action with the fotorama'] = 'fotorama在任何用戶動作後停止幻燈片';
 $lang['Fullscreen navigation style'] = '全屏導航風格';
 $lang['Only fullscreen mode'] = '只有全屏模式';
-?>
+$lang['Add close button'] = '添加關閉按鈕';
+$lang['Add image info button'] = '添加圖片信息按鈕';
+$lang['Replace picture page'] = '更換圖片頁面';
+$lang['Resize to fit window'] = '調整大小以適合窗口';
+$lang['Show caption with image title'] = '顯示標題與圖片標題';
+$lang['Thumbnail height (when present)'] = '縮略圖高度（當存在時）';
+$lang['Thumbnail is a square (when present)'] = '縮略圖是一個正方形（當存在時）';
+$lang['Use crossfade transition on click'] = '在點擊時，使用交叉淡入淡出的過渡';
+$lang['Waiting time before loading a new image'] = '加載新圖像之前的等待時間';
+$lang['except an administrator'] = '除了管理員';
+$lang['milliseconds'] = '毫秒';
